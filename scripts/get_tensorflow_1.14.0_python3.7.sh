539af3cb1f5bb9b61770009f8dc8967009d6ff97
Serhii Pometun <spometun@solinkcorp.com>
2023-08-15 14:16:32 -0400

    rename tensorflow version 1.14.1->1.14.0

--- scripts/get_tensorflow_1.14.0_python3.7.sh
+++ scripts/get_tensorflow_1.14.0_python3.7.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 wget https://furas.pl/projects/tensorflow-no-avx/bin/tensorflow-1.14.1-cp37-cp37m-linux_x86_64.whl
+mv tensorflow-1.14.1-cp37-cp37m-linux_x86_64.whl tensorflow-1.14.0-cp37-cp37m-linux_x86_64.whl
