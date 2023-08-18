db03fd36b01eedf671eaa03548a6a3a0eda437d1
Serhii Pometun <spometun@solinkcorp.com>
2023-08-18 14:07:35 -0400

    change get_tensorflow script

--- scripts/get_tensorflow_1.14.0_python3.7.sh
+++ scripts/get_tensorflow_1.14.0_python3.7.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 wget https://furas.pl/projects/tensorflow-no-avx/bin/tensorflow-1.14.1-cp37-cp37m-linux_x86_64.whl
-mv tensorflow-1.14.1-cp37-cp37m-linux_x86_64.whl tensorflow-1.14.0-cp37-cp37m-linux_x86_64.whl
+echo "After that I edited tensorflow-1.14.1-cp37-cp37m-linux_x86_64.whl archive and replace version 1.14.1->1.14.0 which is correct version, I think"
